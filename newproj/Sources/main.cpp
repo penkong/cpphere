ddeaf1a2099211dd707914410769924485b2be22
mazdak <nazemi.works@gmail.com>
2021-06-03 11:36:19 +0200

    ss

--- newproj/Sources/main.cpp
+++ newproj/Sources/main.cpp
@@ -1,12 +1,71 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-  cout << "      /" << endl;
-  cout << "     / " << endl;
-  cout << "    /  " << endl;
+
+  string chars = "chars";
+
+  int charsAge;
+  charsAge = 18;
+
+  //  data types in c++
+
+  char grade = 'A';
+  string phrase = "Here are these";
+  int age = 44;
+  // float 
+  // for specific 
+  double mld = 2.33434;
+  bool isIt = true;
+
+
+  //  with strings 
+  // cout << phrase.length();
+  // cout << phrase[0];
+  // phrase[0] = 'D';
+  // cout << phrase;
+
+  // cout << phrase.find("are", 0) << endl;
+  // string phase = phrase.substr(5, 3);
+  // cout << phase;
+
+  // numbers
+  // cout << 10.0 / 3.0 << endl;
+
+  // cout << pow(2, 5) << endl;
+  // cout << sqrt(24);
+
+
+  //  get user input
+  // int agged;
+  // cout << "Enter Age: " ;
+  // cin >> agged;
+  // cout << "Your are " << agged << " Years old" << endl;
+
+  // string name;
+  // cout << "Enter Name: ";
+  // getline(cin, name);
+  // cout << "Hello " << name << endl;
+
+  // calculator 
+  // int num1, num2;
+  // cout << "Enter First Number : ";
+  // cin >> num1;
+  // cout << "Enter Second Number : ";
+  // cin >>  num2 ;
+  // cout << num1 + num2;
+
+
+  // mad libs
+
+
+  // cout << mld << endl;
+  // cout << "hello " << chars << endl;
+  // cout << "    /  " << charsAge << endl;
+  // cout << "    " << chars << "/ " << endl;
   
   return 0;
 }

88a82a0476ccfc023eda333c3afa9b868bb4835a
mazdak <nazemi.works@gmail.com>
2021-06-04 07:45:53 +0200

    more on

---
+++ celeb/sl.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// using directive
+// using namespace std;
+
+// using declration
+using std::cout;
+using std::cin;
+using std::endl;
+
+//  read c++ core guidelines
+
+int main() {
+  cout << "hellw";
+  int sl;
+  cin >> sl;
+  // need to say what output format is 
+  printf("%i\n", sl);
+  return 0;
+}
